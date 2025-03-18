7231219c24000b3fc870c012fdcd499209739880
reyhkm <149551621+reyhkm@users.noreply.github.com>
2025-03-19 00:41:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -261,8 +261,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
-});
